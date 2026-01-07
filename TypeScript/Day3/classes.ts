5e18972a252793633a2f482f41f0981bf9521509
Anuj <anujdhole17@gmail.com>
2026-01-07 16:47:03 +0530

    Typescript

--- TypeScript/Day3/classes.ts
+++ TypeScript/Day3/classes.ts
@@ -3,16 +3,17 @@ class Person{
     age:number;
 
     constructor(name:string,age:number){
-        this.name = name;
+        this.name = name; //this.name refers to the variable name of class and =name is the parameter passed to constructor, coud be different names as well like n1
         this.age = age;
     }
-
+//to access variables and methods inside class we use constructor and this keyword
+//this keyword refers to current object
     greet():void{
         console.log(`hi ${this.name}`);
-    }
+    } //no need to write function keyword inside class
 }
 
-// Blueprint of an object
+// class is blueprint of an object
 
 
 const obj1 = new Person("Rohit",20);
@@ -25,6 +26,12 @@ console.log(obj1.name);
 obj1.greet();
 
 // public private protected
+//js doesnt have private and protected access modifiers
+// public: accessible everywhere
+// private: accessible only inside the class (has but implemetation is different)
+// protected: accessible inside the class and its derived classes
+//ts has these access modifiers
+
 
 class Customer{
     public name:string;
@@ -44,11 +51,20 @@ class Customer{
 }
 
 const P1 = new Customer("Deepak",20,420);
-console.log(P1.name);
+console.log(P1.name); //as public
+// console.log(P1.age); // error as private
+// console.log(P1.balance); // error as protected
+//access private and protected members outside the class is not allowed
+//do it via methods inside the class
+// u can make methods public to access private and protected members
+//methods can have access modifiers as well
+//methods are public by default
 
 console.log(P1.meet());
-// console.log(P1.balance);
 
+// inheritance
+// used to inherit properties and methods from one class to another class
+//just like interface extend keyword
 class Employee extends Customer {
     salary:number;
 
@@ -67,31 +83,49 @@ class Employee extends Customer {
     }
 }
 
+//constructor of derived class must call super() which calls constructor of base class else error
+
 const E1 = new Employee(420,"Rohit", 20,320);
-console.log(E1.meet());
-console.log(E1.salary);
+console.log(E1.meet()); //overridden method, calls derived class method, if not present calls base class method
+E1.greet(); //calls derived class method
+console.log(E1.salary); //accessible as public
+console.log(E1.name); //accessible as public
+// console.log(E1.balance); //error as protected member not accessible outside class and derived class
+// console.log(E1.age); //error as private member not accessible outside class
+//friend functions not present in ts
+// no concept of static members in ts
+//static members are shared among all objects of class
+// they belong to class not to object
 
 
-// Generic: Template
+// Generic Template : 
+// to create reusable components
+// to work with any data type
+// to create a component that can work with different data types
+// to create a function or class that can work with any data type
+
+// // without generics
 
 // function value(a:(number|string|number[]|boolean)):(number|string|number[]|boolean){
 //    return a;
 // }
 
-
+//with generics
 function value<T>(a:T):T{
     return a;
 }
+//can use any valid variable name inplace of T like U,V,X etc but T is mostly used
 
-
-console.log(value<number>(10));
+console.log(value<number>(10)); 
+// or just console.log(value(10)); ts infers type automatically
 console.log(value<string>("Rohit"));
 console.log(value([10,11,12,13,14]));
 console.log(value(true));
 console.log(value(["MOhan","Rohan"]))
 
 
-
+//can have multiple generics as well
+//can use generics in interfaces as well
 interface Admin<T,U> {
     name:string,
     age:number,
@@ -115,3 +149,17 @@ const obj11: Admin<string,number>={
 }
 
 
+//how generics differ from rest parameters
+// generics are used to create reusable components that can work with any data type
+// rest parameters are used to represent an indefinite number of arguments as an array
+// generics are used to create a function or class that can work with any data type
+// rest parameters are used to create a function that can take multiple arguments
+
+//how generics differ from union types
+// generics are used to create reusable components that can work with any data type
+// union types are used to represent a value that can be one of several types
+// generics are used to create a function or class that can work with any data type
+// union types are used to create a variable that can hold multiple types
+
+// generics provide type safety at compile time
+// union types provide type safety at runtime
--- TypeScript/Day3/first.ts
+++ TypeScript/Day3/first.ts
@@ -4,6 +4,7 @@
 //     gender:string,
 //     aadhar?:number
 // }
+// // // ? makes the property optional, so we can skip it while creating object
 
 
 // const obj:Person = {
@@ -26,21 +27,41 @@
 //     balance:210,
 //     age:20
 // }
+// // obj2.age = 21; // error as age is readonly
 
+
+// <!-- Utility Types for Objects -->
 // // Partial: All property becomes optional
 // // Required: ALl property should be filled
-// // Readonly: The property can only be read, write option is not available
+// // Readonly: The property can only be read, write option is not available i.e cant be modified
 
 
 // // array of Objects
 
-// interface peopele {name:string,age:number};
+// const arr = [{name:"Rohit", age:20}, {name: "Mohit", age:18}];
+//automatically infers the type of array elements
+//OR
+// const arr: {name:string, age:number}[] = [{name:"Rohit", age:20}, {name: "Mohit", age:18}]; 
+// explicitly defining the type of array elements
+//OR
+
+// interface people {name:string,age:number};
+//const arr: people[] = [{name:"Rohit",age:20},{name:"Mohit",age:18}];
+
+// // union type in array of objects
 // interface manager {salary:number,id:string}
 
-// const arr: (peopele | manager)[] = [{name:"Rohit",age:20},{name:"Mohit",age:18}, {salary:20,id:"2321"}]
+// const arr: (people | manager)[] = [{name:"Rohit",age:20},{name:"Mohit",age:18}, {salary:20,id:"2321"}]
 
 // // function in TS
 
+//in js
+// function greet(a){
+//     console.log(a);
+//     return a+5;
+// }
+//will give error as it will infer a as any type
+//mention return type as well tho optional cuz ts infers it automatically
 
 // function greet(a:number):number{
 //     console.log(a);
@@ -56,13 +77,14 @@
 
 
 // meet("Anshika Verma", 4);
+
 // // default parameter
 // function neet(msg:string = "Jit"){
 //     console.log(msg);
 // }
 
-// neet();
-// neet("Bittu");
+// neet(); //if no argument is passed, default value is taken
+// neet("Bittu"); //if argument is passed, it overrides the default value
 
 
 // // Optional Parameter
@@ -71,11 +93,17 @@
 //     console.log(person||"Mohan");
 // }
 
-// GATE("Rohit");
-// GATE();
+// GATE("Rohit"); // output: Rohit
+// GATE(); // output: Mohan
 
 
 // // arrow function
+
+// const sum = (a,b) =>
+// {
+//     return a+b;
+// }
+//gives error as types are not defined
 
 // const sum = (a:number,b:number):number=>{
 //     return a+b;
@@ -87,15 +115,24 @@
 // // callback function
 
 
-// type chill = (amount:number)=>void;
 
-// const sqaureroot = (val:number)=>{
+
+// const squareroot = (val:number):=>{
 //    return val*val;
 // }
 
 
 
-// function placeOrder(order:number,callback: chill):void{
+// function placeOrder(order:number,callback: {amount:number} => void{
+    
+//     const amount:number = order+10;
+//     callback(amount);
+// }
+
+//or
+
+// type chill = (amount:number)=>void;
+// function placeOrder(order:number,callback:chill){
     
 //     const amount:number = order+10;
 //     callback(amount);
@@ -111,7 +148,7 @@
 // // Rest Parameter
 
 
-// function total(...arr:number[]){
+// function total(...arr:number[]){ // arr is an array of numbers
 //    let ans:number = 0 ;
 //    arr.forEach((val:number)=>ans = ans+val);
 //    console.log(ans);    
@@ -125,6 +162,8 @@
 
 
 // // extend keyword
+// used to inherit properties and methods from one class to another class
+//can do this without extend keyword as well but that wont be inheritance
 
 // interface human{
 //     name:string,
